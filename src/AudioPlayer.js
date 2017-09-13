8d5c7621aa457e82970b055366d1efbaf83ea4dd
Jacob Greenwald <jeg4t@virginia.edu>
2017-09-13 17:43:27 -0400

    add start of audio player

---
+++ src/AudioPlayer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AudioPlayer extends Component {
+  render () {
+    return (
+      <div>
+        <p>audio player coming</p>
+      </div>
+    )
+  }
+}
+
+export default AudioPlayer
