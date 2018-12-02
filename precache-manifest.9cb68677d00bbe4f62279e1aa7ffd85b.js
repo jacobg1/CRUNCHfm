bc602488a9e7ab76da99562f2a6e4faea9080ec5
Jacob Greenwald <jeg4t@virginia.edu>
2018-12-02 12:56:12 -0500

    Updates

---
+++ precache-manifest.9cb68677d00bbe4f62279e1aa7ffd85b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3f8ea4005701c0324c6d60a58a972d5",
+    "url": "/CRUNCHfm/static/media/album_icon.e3f8ea40.png"
+  },
+  {
+    "revision": "cbe7c3cc73a74b9504e3",
+    "url": "/CRUNCHfm/static/css/main.b92a6f8b.chunk.css"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/CRUNCHfm/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "9274f76aaa462f8e5326",
+    "url": "/CRUNCHfm/static/js/1.9274f76a.chunk.js"
+  },
+  {
+    "revision": "f846b806fb28f76916a1",
+    "url": "/CRUNCHfm/static/js/runtime~main.f846b806.js"
+  },
+  {
+    "revision": "239bc9244e5055c80480f07402f1b3eb",
+    "url": "/CRUNCHfm/static/media/music_icon.239bc924.png"
+  },
+  {
+    "revision": "cbe7c3cc73a74b9504e3",
+    "url": "/CRUNCHfm/static/js/main.cbe7c3cc.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/CRUNCHfm/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/CRUNCHfm/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/CRUNCHfm/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/CRUNCHfm/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "9274f76aaa462f8e5326",
+    "url": "/CRUNCHfm/static/css/1.bf174ed7.chunk.css"
+  },
+  {
+    "revision": "47b8a8af557ea42f3a74c5a80e189cd1",
+    "url": "/CRUNCHfm/index.html"
+  }
+];
